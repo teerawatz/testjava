b67ff8b5c6621bc6954c0701bb3688031a67e697
teerawatz <teerawat.notz@gmail.com>
2024-01-11 16:34:36 +0700

    api with custom query

--- src/main/java/com/example/testjava/controller/EmployeeController.java
+++ src/main/java/com/example/testjava/controller/EmployeeController.java
@@ -1,6 +1,8 @@
 package com.example.testjava.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.testjava.model.Employee;
@@ -71,6 +74,12 @@ public class EmployeeController {
     public ResponseEntity<Employee> deleteEntity(@PathVariable Long id) {
         employeeRepository.deleteById(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/name")
+    public List<Employee> getEntitiesByName(@RequestParam String name) {
+        
+        return employeeRepository.findByName(name);
     }
     
 }
--- src/main/java/com/example/testjava/repository/EmployeeRepository.java
+++ src/main/java/com/example/testjava/repository/EmployeeRepository.java
@@ -1,12 +1,17 @@
 package com.example.testjava.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.testjava.model.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     // You can add custom queries here if needed
-    //Optional<Employee> findById(Long id);
+    @Query("SELECT e FROM Employee AS e WHERE e.name LIKE %:name%")
+    List<Employee> findByName(@Param("name") String name);
+    
 }
